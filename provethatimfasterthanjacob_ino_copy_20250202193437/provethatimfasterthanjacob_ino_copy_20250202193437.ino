4d61e850caba12d5b295afcc53606e5c13590904
nzfine3 <nzfine3@gmail.com>
2025-02-02 19:52:20 -0500

    Adding comments
    Adding comments so that is is more understandable.

--- provethatimfasterthanjacob_ino_copy_20250202193437/provethatimfasterthanjacob_ino_copy_20250202193437.ino
+++ provethatimfasterthanjacob_ino_copy_20250202193437/provethatimfasterthanjacob_ino_copy_20250202193437.ino
@@ -1,4 +1,4 @@
-#include <LiquidCrystal.h>
+// declare constants for ultrasonic sensor
 
 const int trigPin = 9;
 const int echoPin = 10;
@@ -6,14 +6,21 @@ const int swPin = 2;
 const int dist = 13;
 const int buttonPin = 2;
 float duration, distance;
+
+// declare time for s = d/t
 float time = 0;
-int buttonpresses = 0;
+
+// declare a state for if the button has been pressed or not
 bool timestate = false;
+
+// declare speed for printing to outputs
 int speed;
 
 
 
 void setup() {
+
+  // adding neccesary pinmodes
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(swPin, INPUT_PULLUP);
@@ -22,33 +29,45 @@ void setup() {
   pinMode(11, OUTPUT);
   pinMode(8, OUTPUT);
   pinMode(7, OUTPUT);
+
+  // start baud rate to 9600
   Serial.begin(9600);
 }
 
 void loop() {
+
+  // send out a wave with a length of 12 microseconds
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
+  // read whether the button has been pressed or not
   int buttonState = digitalRead(buttonPin);
 
+  // calculate distance from ultrasonic sensor
   duration = pulseIn(echoPin, HIGH);
   distance = (duration*.0343)/2;
 
+  // start timer if button has been pressed
   if (buttonState == LOW) {
     timestate = true;
-    buttonpresses = buttonpresses + 1;
   }
   if (timestate == true){
     time = time + 0.1;
   }
-  if (distance < 10) {
+
+  // printing speed if hand is close to ultrasonic sensor
+  if (distance < 15) {
+    // preform speed = distance / time calculation and convert to mph by multiplying by 2.237
     float speed = (dist / time) * 2.237;
+
+    // print speed to serial monitor
     Serial.print("Average Speed: ");
     Serial.print(speed);
 
+    // give output so serial monitor is not required (on a scale from 1 - 7.5 with 5 ticks, 0 - 1.5, 1.5 - 3, 3 - 4.5, 4.5 - 6, and 6 - infinity)
     if (speed < 1.5){
       digitalWrite(7, HIGH);
     } 
@@ -71,9 +90,13 @@ void loop() {
       digitalWrite(13, HIGH);
     }
 
+    // pause code until button is pressed again
     while (true){
-      ;
+      if (buttonState == LOW) {
+        break;
+      }
     }
   }
+  // delay for timer(you need to keep it at 100 milliseconds or else timer will be messed up)
   delay(100);
 }
